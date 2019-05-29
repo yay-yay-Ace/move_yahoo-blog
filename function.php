5cbc24e2d68c6ac4a42615fe0b6241ee9435e198
yay-yay-Ace <51147555+yay-yay-Ace@users.noreply.github.com>
2019-05-29 14:01:06 +0900

    Create function.php

---
+++ function.php
@@ -0,0 +1,5 @@
+function my_scripts() {
+  wp_enqueue_style( 'style-name', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all' );
+  wp_enqueue_script( 'script-name', get_template_directory_uri() . '/js/setting.js', array( 'jquery' ), '1.0.0', true );
+}
+add_action( 'wp_enqueue_scripts', 'my_scripts' );
